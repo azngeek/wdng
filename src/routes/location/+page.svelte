22e66095caf25d776d51c23f2c4b53f77490afbd
donbosco <vanhoi@mothership.de>
2023-01-23 19:51:08 +0100

    Lustiges Favicon ergänzt

--- src/routes/location/+page.svelte
+++ src/routes/location/+page.svelte
@@ -1,32 +1,37 @@
 <script lang="ts">
-    
 	import { onMount } from 'svelte';
-    import { Loader } from '@googlemaps/js-api-loader';
+	import { Loader } from '@googlemaps/js-api-loader';
 
 	onMount(() => {
-
-        let map: google.maps.Map;
+		let map: google.maps.Map;
 
 		const loader = new Loader({
 			apiKey: 'AIzaSyDTmK_YB6FWixG9BczztAcaV6WtNZgVatY',
 			version: 'weekly'
-        });
+		});
 
-        var grayStyles = [
-        {
-          featureType: "all",
-          stylers: [
-            { saturation: -10 },
-            { lightness: 50 }
-          ]
-        },
-      ];
+		var grayStyles = [
+			{
+				featureType: 'all',
+				stylers: [{ saturation: -20 }, { lightness: 30 }]
+			}
+		];
 
 		loader.load().then(() => {
+			const loc = { lat: 47.7063905, lng: 11.8763645 };
+
 			map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
-				center: { lat: -34.397, lng: 150.644 },
-				zoom: 8,
-                styles: grayStyles
+				center: loc,
+				zoom: 12,
+				styles: grayStyles
+			});
+
+			const image =
+				'./wedding-couple.png';
+			const beachMarker = new google.maps.Marker({
+				position: loc,
+				map,
+				icon: image
 			});
 		});
 	});
@@ -36,15 +41,22 @@
 <h2 class="text-2xl mt-4">Markus Wasmeier Freilichtmuseum</h2>
 <h3 class="text-2xl font-light">August 12, 2023</h3>
 
-<p class="font-normal italic mt-4">Model Bavarian village with themed tours, costumed craftsmen & a biergarten with mountain views</p>
-
-<p class="font-normal mt-4">
-    The Markus Wasmeier Freilicht Museum is an open-air museum located in Schliersee, Germany. The museum is dedicated to showcasing the traditional culture and lifestyle of the region, as well as the history of skiing and winter sports. Visitors can explore the museum's collection of over 50 historical buildings, including a traditional farm, a sawmill, and a blacksmith's shop. The museum also features a variety of exhibits, including a display of vintage ski equipment and a collection of traditional folk costumes. In addition, the museum offers a variety of activities, such as guided tours, workshops, and special events. The Markus Wasmeier Freilicht Museum is a popular destination for visitors interested in the history and culture of the region.
+<p class="font-normal italic mt-4">
+	Model Bavarian village with themed tours, costumed craftsmen & a biergarten with mountain views
 </p>
 
+<p class="font-normal mt-4">
+	The Markus Wasmeier Freilicht Museum is an open-air museum located in Schliersee, Germany. The
+	museum is dedicated to showcasing the traditional culture and lifestyle of the region, as well as
+	the history of skiing and winter sports. Visitors can explore the museum's collection of over 50
+	historical buildings, including a traditional farm, a sawmill, and a blacksmith's shop. The museum
+	also features a variety of exhibits, including a display of vintage ski equipment and a collection
+	of traditional folk costumes. In addition, the museum offers a variety of activities, such as
+	guided tours, workshops, and special events. The Markus Wasmeier Freilicht Museum is a popular
+	destination for visitors interested in the history and culture of the region.
+</p>
 
-
-<div id="map" class="max-w-screen-lg h-64 mt-4 z-0" />
+<div id="map" class="max-w-screen-lg h-96 mt-4 z-0" />
 
 <style>
 </style>
