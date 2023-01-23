4ea06ab6b197aa92153917315b8fc85d6ce00bb4
donbosco <vanhoi@mothership.de>
2023-01-23 20:32:56 +0100

    Timeline comment

--- src/routes/agenda/+page.svelte
+++ src/routes/agenda/+page.svelte
@@ -1,32 +1,44 @@
-<div class="container mx-auto">
-    <div class="p-10 ">
-      <div class="border-2-2 border-opacity-20 border" style="left: 50%"></div>
+<h1 class="text-l">Timeline</h1>
+<h2 class="text-2xl mt-4">Blabbla</h2>
+<h3 class="text-2xl font-light">August 12, 2023</h3>
 
-      <!-- right timeline -->
-      <div class="mb-8 md:flex md:justify-between items-center ">
-        <div class="order-1 md:w-5/12"></div>
-        <div class="hidden md:flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-          <h1 class="mx-auto font-semibold text-lg text-white">1</h1>
-        </div>
-        <div class="order-1 bg-green-100 shadow-xl md:w-5/12 px-6 py-4">
-          <h3 class="mb-3 font-bold text-gray-800 text-xl">Lorem Ipsum</h3>
-          <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-        </div>
-      </div>
-  
-      <!-- left timeline -->
-      <div class="mb-8 md:flex justify-between flex-row-reverse items-center w-full">
-        <div class="order-1 md:w-5/12"></div>
-        <div class="hidden md:flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-          <h1 class="mx-auto text-white font-semibold text-lg">2</h1>
-        </div>
-        <div class="order-1 bg-red-100 shadow-xl md:w-5/12 px-6 py-4">
-          <h3 class="mb-3 font-bold text-black text-xl">Lorem Ipsum</h3>
-          <p class="text-sm font-medium leading-snug tracking-wide text-black text-opacity-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-        </div>
-      </div>
-      
-          
+<p class="font-normal italic mt-4">
+	Blablablaab
+    balbal
+</p>
 
-    </div>
-  </div>
+<div class="container mx-auto mt-8">
+	<div class="border-2-2 border-opacity-20 border" style="left: 50%" />
+
+	<!-- right timeline -->
+	<div class="mb-8 md:flex md:justify-between items-center ">
+		<div class="order-1 md:w-5/12" />
+		<div class="hidden md:flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
+			<h1 class="mx-auto font-semibold text-lg text-white">1</h1>
+		</div>
+		<div class="order-1 bg-green-100 shadow-xl md:w-5/12 px-6 py-4">
+			<h3 class="mb-3 font-bold text-gray-800 text-xl">Lorem Ipsum</h3>
+			<p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+				been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+				galley of type and scrambled it to make a type specimen book.
+			</p>
+		</div>
+	</div>
+
+	<!-- left timeline -->
+	<div class="mb-8 md:flex justify-between flex-row-reverse items-center w-full">
+		<div class="order-1 md:w-5/12" />
+		<div class="hidden md:flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
+			<h1 class="mx-auto text-white font-semibold text-lg">2</h1>
+		</div>
+		<div class="order-1 bg-red-100 shadow-xl md:w-5/12 px-6 py-4">
+			<h3 class="mb-3 font-bold text-black text-xl">Lorem Ipsum</h3>
+			<p class="text-sm font-medium leading-snug tracking-wide text-black text-opacity-100">
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+				been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+				galley of type and scrambled it to make a type specimen book.
+			</p>
+		</div>
+	</div>
+</div>
