3892bfe6e690c61c8c2679db758f0352a587ea5e
donbosco <vanhoi@mothership.de>
2023-02-21 21:20:45 +0100

    cropped image

--- src/routes/best_man_maid/+page.svelte
+++ src/routes/best_man_maid/+page.svelte
@@ -60,15 +60,31 @@
     <p>{$t('best_man_maid_of_honor.man.paragraph_5')}</p>
     <p>{$t('best_man_maid_of_honor.man.paragraph_6')}</p>
 
-    <div class="mt-8 text-center prose prose-img:rounded-xl max-w-screen-lg">
-		<img 
-		class="md:w-5/5 mx-auto"
-		src="/dubai.jpeg"
-		alt="wasmeier_path"
-		/>
-		<div class="md:w-4/5 mx-auto text-right text-xs mt-0">
-		  <span></span>
-		</div>
-	</div>
+
+    <section class="overflow-hidden text-neutral-700">
+      <div class="container mx-auto px-5 py-2 ">
+        <div class="-m-1 flex flex-wrap md:-m-2">
+          <div class="flex w-2/4 flex-wrap">
+            <div class="w-full p-1 md:p-2">
+              <img
+                alt="gallery"
+                class="block h-full w-full object-cover object-center"
+                src="CIMG1364.jpeg" />
+            </div>
+          </div>
+ 
+          <div class="flex w-2/4 flex-wrap">
+            <div class="w-full p-1 md:p-2">
+              <img
+                alt="gallery"
+                class="block h-full w-full object-cover object-center"
+                src="dubai.jpeg" />
+            </div>
+          </div>
+         
+        </div>
+      </div>
+    </section>
+
 	
 </article>
