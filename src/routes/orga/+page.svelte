d9fc97f4422467897e2f1bdd5fc147a72f9535b5
donbosco <vanhoi@mothership.de>
2023-08-12 07:56:19 +0200

    Rede + Orga

---
+++ src/routes/orga/+page.svelte
@@ -0,0 +1,210 @@
+<script lang="ts">
+	import Divider from '$lib/components/Divider.svelte';
+	import {t, locale} from '$lib/i18n';
+</script>
+
+<h2 class="text-2xl mt-4">{$t('agenda.header')}</h2>
+
+<article class="prose max-w-screen-lg mt-8">
+
+	<div class="container mx-auto mt-8">
+
+		<div class="border-2-2 border-opacity-20 border" style="left: 50%" />
+
+
+        <!-- 14:00 -->
+        <div class="mb-8 mt-8 md:flex md:justify-between items-center">
+            <div class="grid grid-cols-12 shadow md:px-6 md:py-2 w-full">
+                <div class="col-span-9 md:col-span-10 w-full">
+                    <h3 class="mb-3 font-bold text-gray-800 text-xl">14:00 Ankunft Gäste</h3>
+                    <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+                            <li>Empfang: Bosco, Olga, Lara, Constanze</li>
+                            <li>Sicherstellen, dass Sebi und Amandine smalltalken können.</li>
+                            <li>Gäste über Ablagemöglichkeiten für Garderobe informieren</li>
+                            <li>Gäste sollen um spätestens 14:25 Platz nehmen</li>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+         <!-- 14:30 -->
+        <div class="mb-8 mt-8 md:flex md:justify-between items-center">
+            <div class="grid grid-cols-12 shadow md:px-6 md:py-2 w-full">
+                <div class="col-span-9 md:col-span-10 w-full">
+                    <h3 class="mb-3 font-bold text-gray-800 text-xl">14:30 - freie Trauung</h3>
+                    <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+                            <li>Iris beginnt pünktlich mit der Rede</li>
+                            <li>Sicherstellen, dass Emilie das Eheversprechen auf Französisch verteilt hat</li>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- 15:30 -->
+        <div class="mb-8 mt-8 md:flex md:justify-between items-center">
+            <div class="grid grid-cols-12 shadow md:px-6 md:py-2 w-full">
+                <div class="col-span-9 md:col-span-10 w-full">
+                    <h3 class="mb-3 font-bold text-gray-800 text-xl">15:30 - Hochzeitsempfang</h3>
+                    <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+                        <li>Hochzeitsempfang mit Drinks und Fingerfood (Museumsbrot/ Champagne/ Wein/ Wasser/ Kaffee/Eis)</li>
+                        <li>Start Live Bayerische Musik</li>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- 16:30 -->
+        <div class="mb-8 mt-8 md:flex md:justify-between items-center">
+            <div class="grid grid-cols-12 shadow md:px-6 md:py-2 w-full">
+                <div class="col-span-9 md:col-span-10 w-full">
+                    <h3 class="mb-3 font-bold text-gray-800 text-xl">16:30 - 17:00 Photoshooting</h3>
+                    <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+                        <li>Jeweiligen Teams organisieren Fotoshootings</li>
+                        <li>Insgesamt 9 Gruppen - Straffes Timing, da die nächste Aktivität um 17:15 ist!</li>
+                        <li><a href="https://docs.google.com/spreadsheets/d/1Oo1gSEOYWDqbQ42FuM-gVahn0Khc8Qu-DVBcK_z3z6M/edit#gid=0">Link zur Tabelle</a></li>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- 17:00 -->
+        <div class="mb-8 mt-8 md:flex md:justify-between items-center">
+            <div class="grid grid-cols-12 shadow md:px-6 md:py-2 w-full">
+                <div class="col-span-9 md:col-span-10 w-full">
+                    <h3 class="mb-3 font-bold text-gray-800 text-xl">17:00 - Box mit Geld leeren</h3>
+                    <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+                        <li>Box mit Geld ins Sebis Auto bringen und leeren</li>
+                        <li>Box zurück an den Platz bringen.</li>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- 17:15 -->
+        <div class="mb-8 mt-8 md:flex md:justify-between items-center">
+            <div class="grid grid-cols-12 shadow md:px-6 md:py-2 w-full">
+                <div class="col-span-9 md:col-span-10 w-full">
+                    <h3 class="mb-3 font-bold text-gray-800 text-xl">17:15 - Start Aktivität</h3>
+                    <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+                        <li>Achtung: Man kann NUR Bierverkostung ODER die Museumstour machen. Der Text auf Deutsch ist falsch.</li>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+         <!-- 18:00 -->
+        <div class="mb-8 mt-8 md:flex md:justify-between items-center">
+            <div class="grid grid-cols-12 shadow md:px-6 md:py-2 w-full">
+                <div class="col-span-9 md:col-span-10 w-full">
+                    <h3 class="mb-3 font-bold text-gray-800 text-xl">18:00 - Fotos Sebi und Amandine</h3>
+                    <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+                        <li>Sicherstellen, dass Sebi und Amandine ihre Paarbilder machen :)</li>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- 18:15 -->
+        <div class="mb-8 mt-8 md:flex md:justify-between items-center">
+            <div class="grid grid-cols-12 shadow md:px-6 md:py-2 w-full">
+                <div class="col-span-9 md:col-span-10 w-full">
+                    <h3 class="mb-3 font-bold text-gray-800 text-xl">18:15 - Ende Aktivität</h3>
+                    <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+                        <li>Nach dem Ende der Aktivität sollen die Gäste in die Tenne geführt werden.</li>
+                        <li>Aufgabe muss an das gesamte Orga-Team verteilt werden, da dies zeitkritisch ist.</li>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+         <!-- 18:40 -->
+        <div class="mb-8 mt-8 md:flex md:justify-between items-center">
+            <div class="grid grid-cols-12 shadow md:px-6 md:py-2 w-full">
+                <div class="col-span-9 md:col-span-10 w-full">
+                    <h3 class="mb-3 font-bold text-gray-800 text-xl">18:40 - Brautpaar kommt</h3>
+                    <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+                        <li>Sebi und Amandine kommen in die Tenne</li>
+                        <li>Spätestens jetzt sitzen alle Gäste</li>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- 19:00 -->
+        <div class="mb-8 mt-8 md:flex md:justify-between items-center">
+            <div class="grid grid-cols-12 shadow md:px-6 md:py-2 w-full">
+                <div class="col-span-9 md:col-span-10 w-full">
+                    <h3 class="mb-3 font-bold text-gray-800 text-xl">19:00 - Hochzeitsdinner</h3>
+                    <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+                        <li>Eröffnungsrede</li>
+                        <li>Beginn Hochzeitsdinner</li>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+         <!-- 19:45 -->
+        <div class="mb-8 mt-8 md:flex md:justify-between items-center">
+            <div class="grid grid-cols-12 shadow md:px-6 md:py-2 w-full">
+                <div class="col-span-9 md:col-span-10 w-full">
+                    <h3 class="mb-3 font-bold text-gray-800 text-xl">19:45 - Zwischengang</h3>
+                    <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+                        <li>Weitere Reden möglich</li>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- 20:45 -->
+        <div class="mb-8 mt-8 md:flex md:justify-between items-center">
+            <div class="grid grid-cols-12 shadow md:px-6 md:py-2 w-full">
+                <div class="col-span-9 md:col-span-10 w-full">
+                    <h3 class="mb-3 font-bold text-gray-800 text-xl">20:45 - Hauptgang</h3>
+                    <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+                        <li>Weitere Reden möglich</li>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+         <!-- 21:30 -->
+        <div class="mb-8 mt-8 md:flex md:justify-between items-center">
+            <div class="grid grid-cols-12 shadow md:px-6 md:py-2 w-full">
+                <div class="col-span-9 md:col-span-10 w-full">
+                    <h3 class="mb-3 font-bold text-gray-800 text-xl">21:30 - Hochzeitskuchen</h3>
+                    <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+                        <li>Mit der Band und der Küche soll der Kuchen zeitnah serviert werden</li>
+                        <li>Dessert-Buffet wird eröffnet</li>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- 22:00 -->
+        <div class="mb-8 mt-8 md:flex md:justify-between items-center">
+            <div class="grid grid-cols-12 shadow md:px-6 md:py-2 w-full">
+                <div class="col-span-9 md:col-span-10 w-full">
+                    <h3 class="mb-3 font-bold text-gray-800 text-xl">22:00 - Long-Drinks</h3>
+                    <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+                        <li>Party!</li>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- 24:00 -->
+        <div class="mb-8 mt-8 md:flex md:justify-between items-center">
+            <div class="grid grid-cols-12 shadow md:px-6 md:py-2 w-full">
+                <div class="col-span-9 md:col-span-10 w-full">
+                    <h3 class="mb-3 font-bold text-gray-800 text-xl">24:00 - Midnight-Snacks</h3>
+                    <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+                        <li>Ein Mitternachtssnack steht zur Stärkung bereit</li>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+
+
+	</div>
+</article>
---
+++ src/routes/speech/+page.svelte
@@ -0,0 +1,150 @@
+<script lang="ts">
+	import Divider from '$lib/components/Divider.svelte';
+	import {t, locale} from '$lib/i18n';
+</script>
+
+<h2 class="text-2xl mt-4">{$t('agenda.header')}</h2>
+
+<article class="prose max-w-screen-lg mt-8">
+
+	<div class="container mx-auto mt-8">
+
+		<div class="border-2-2 border-opacity-20 border" style="left: 50%" />
+
+
+        <!-- 14:00 -->
+        <div class="mb-8 mt-8 md:flex md:justify-between items-center">
+            <div class="grid grid-cols-12 shadow md:px-6 md:py-2 w-full">
+                <div class="col-span-9 md:col-span-10 w-full">
+                    <h3 class="mb-3 font-bold text-gray-800 text-xl">14:00 Sp</h3>
+                    <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+
+- ding ding -
+Thank you for your attention.
+
+My Name is Don Bosco. I am mostly introduced as the 'it guy' as my professional services include software development but i also do wedding websites and repair your printer if you ask me nicely. And Sebi and Amandine asked me very nicely. And i am also Sebis Best Man! And as the best man it is my duty to tell you all of Sebis little secrets - actually i got asked yesterday to tell some stories. But as the Best Man i refused to and instead put all of them online. They are now available at the wedding webiste. You will find it at the FAQ-section. It is the veeery laaasst link. But please do not click if you consider yourself are a good friend or guest :)
+
+- Pause -
+
+I have the privilegue to hold a speech tonight and i spared nooo effort and so i decided to use ChatGPT (thanks for this great tip, david - the australian guy). So i want to tell you the Story about Adam and Stefannie.
+
+Jokes aside - A very warm welcome every. We are happy to have you here and appreciate your attendance. We do have guests from all over the world like of course Switzerland, Australia also Baden Würtemberg (aka The Länd). I would like to bring my first toast by telling you how fantastic you guys look tonight! This day would not be possible withouth you. Thanks for coming to Schliersee and celebrating with us. Cheers, Pröschtli, Prost and Sante.
+
+That's it for the international part: I am very sorry to tell you that the rest of the speech is in german but it will also be very short - so meanwhile maybe check the homepage .. :)
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- 15:30 -->
+        <div class="mb-8 mt-8 md:flex md:justify-between items-center">
+            <div class="grid grid-cols-12 shadow md:px-6 md:py-2 w-full">
+                <div class="col-span-9 md:col-span-10 w-full">
+                    <h3 class="mb-3 font-bold text-gray-800 text-xl">15:30 - Hochzeitsempfang</h3>
+                    <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+                        Bevor wir in die charmante Welt von Sebi eintauchen, lasst uns sicherstellen, dass unsere Getränke gefüllt sind, denn wir haben eine Menge wunderbarer Geschichten und Fakten zu teilen! 🥂
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- 16:30 -->
+        <div class="mb-8 mt-8 md:flex md:justify-between items-center">
+            <div class="grid grid-cols-12 shadow md:px-6 md:py-2 w-full">
+                <div class="col-span-9 md:col-span-10 w-full">
+                    <h3 class="mb-3 font-bold text-gray-800 text-xl">16:30 - 17:00 Photoshooting</h3>
+                    <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+                        Eine Hochzeit ist auch immer eine tolle Gelegenheit, etwas mehr über den Bräutigam zu erfahren. Und um ehrlich zu sein,
+                        ist es mir wirklich schwer gefallen, etwas zu finden, was Sebi mit wenigen Worten gut beschreiben würde. Also erzähle ich einfach etwas aus Sebis Leben.
+
+                        Kennengelert habe ich den Sebi, da war ich ungefähr 14, das müsste also knapp 10 Jahr her sein. Und da hat er dieses eine Video-Spiel gespielt. Das hieß Ice Hokey auf dem NES. Und der Sebi hat nicht nur einfach mit mir ein Videospiel gespielt, nein das war für ihn ein Wettkampf - Ich habe schon früh gemerkt, dass er jemand ist, der auch gerne sehr ehrgeizig sein kann - zumindest wenn es um Sachen ging, die ihm wichtig waren. Eine Eigenschaft, die sogar dazu geführt hat, dass er es im Tetris in die Top 50 Weltrangliste geschafft hat.  </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- 16:30 -->
+        <div class="mb-8 mt-8 md:flex md:justify-between items-center">
+            <div class="grid grid-cols-12 shadow md:px-6 md:py-2 w-full">
+                <div class="col-span-9 md:col-span-10 w-full">
+                    <h3 class="mb-3 font-bold text-gray-800 text-xl">Sebi 90s</h3>
+                    <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+
+                    Und das war Sebi mit 14. Und später so von 16-18, also in den 90er, Wisst ihr noch was für Frisuren man in den 90er hatte? Entweder man sah aus wie Nick-Carter, also so ein Boyband-Mitglied und hatte einen Mittelscheitel oder man sah aus wie Scooter, das war der Typ der die ganze Zeit Hyper Hyper geschriehen hat und aussah wie ein Mann mit einem Igel auf dem Kopf. Für alle Schweizer: Der Nick-Caarter war sowas wie DJ-Bobo - nur in cooler. Und was denkt ihr? War der Sebi eher "Backstreets Boy" oder "Hyper Hyper". Ja der Sebi war der Typ mit der Stachelfrisur auf dem Kopf.
+
+                    </p>
+                 </div>
+            </div>
+        </div>
+
+
+        <!-- Jein -->
+        <div class="mb-8 mt-8 md:flex md:justify-between items-center">
+            <div class="grid grid-cols-12 shadow md:px-6 md:py-2 w-full">
+                <div class="col-span-9 md:col-span-10 w-full">
+                    <h3 class="mb-3 font-bold text-gray-800 text-xl">Jein</h3>
+                    <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+
+Auf jeden Fall hat sich Sebi auch sehr für Musik interessiert, zumindest hat er sehr gerne Deuschen Hip Hop gehört und ich kenne keinen Menschen, der so viele Lieder auswendig kennt. Er kann sogar Gedichte aufsagen, eine Eigenschaft, mit der er Amandine verzaubert hat. Aber ein Lied, dass kann er besonders gut: Und zwar von Fettes-Brot - wer kennt es? - JEIN.
+
+Auf jeder Hochzeit wo dieses Lied läuft, sing er mit. Und hätte sich der Sebi nicht die Versicherungsbranche gewählt - wäre er möglicherweise Rapper geworden. Und damit geht mein zweiter Toast an Frank und Flo, die ihm eine Karriere in der Versicherungsbranche ermöglicht haben.
+
+In diesem Lied, also JEIN, da geht es darum, dass man schwierige Entscheidungen treffen muss und es Konflikte zwischen besten Freunden geben kann. Und Konflikte mit Sebi, die gab es so gut wie nie - zumindest nicht wenn es um Frauen ging. Stattdessen haben ich mit Sebi Teile der Welt bereist. Unter anderem hat er mir bei meiner ersten Auto-Fahrt in den USA fast das Leben gerettet, weil ich nicht wusste, dass die LKWs dort einen auch auf der linken Spur überholen. Man redet über Frauen, geht zusammen feiern und irgendwann gründet man eine WG. Und wenn man mit dem Sebi zusammen wohnt, dann merkt man, welche guten Eigenschaften er besitzt: Er hat einen sehr hohen Gerechtigkeitssinn, ist extrem sozial und großzügig, sehr kompromissbereit und manchmal auch ganz witzig.
+                    </p>
+                 </div>
+            </div>
+        </div>
+
+
+         <!-- Tänzer -->
+        <div class="mb-8 mt-8 md:flex md:justify-between items-center">
+            <div class="grid grid-cols-12 shadow md:px-6 md:py-2 w-full">
+                <div class="col-span-9 md:col-span-10 w-full">
+                    <h3 class="mb-3 font-bold text-gray-800 text-xl">Tänzer</h3>
+                    <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+
+Und wenn wir von Großzügigkeit sprechen, dann ist Sebi der beste Gastgeber, den man sich vorstellen kann. Er sorgt nicht nur dafür, dass alle eine großartige Zeit haben, sondern teilt auch gerne seine Freude und positive Energie - und nicht zuletzt seine Ratschläge und Witze - eine positive Eigenschaft, die er höchstwahrscheinlich von seiner Mutter hat.
+
+Und außerdem ist er auch ein großartiger Tänzer, wie er später beweisen wird - was ihm auch den Spitznamen "Der Dirrigent" eingebracht hat – keine Party ist komplett ohne seinen taktvollen Einsatz.
+                    </p>
+                 </div>
+            </div>
+        </div>
+
+         <!-- Wiesn -->
+        <div class="mb-8 mt-8 md:flex md:justify-between items-center">
+            <div class="grid grid-cols-12 shadow md:px-6 md:py-2 w-full">
+                <div class="col-span-9 md:col-span-10 w-full">
+                    <h3 class="mb-3 font-bold text-gray-800 text-xl">Wiesn</h3>
+                    <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+
+Und wenn wir schon bei Party sind, es gibt keine größere Party in München als das das Oktoberfest.
+Sebi hat das Oktoberfest in unserem Freundeskreis zur Pflichtveranstaltung erklärt – er ist unser wahrer Oktoberfest-Botschafter! Wenn jemand den Spaßfaktor eines Bierkruges steigern kann, dann ist er das.
+
+Und mit der gleichen Leidenschaft zum Oktoberfest, hat er auch seine große Liebe zufälligerweise auf dem Oktoberfest gefunden - und Amandine durfte noch in der gleichen Woche fast die gesamte Familie kennenlernen. Wobei man sagen muss, dass sie mit ihrem Charme leichtes Spiel hatte. Und ich bin sehr froh darüber, dass sie zum richtigen Zeit am richtigen Ort war.
+
+                    </p>
+                 </div>
+            </div>
+        </div>
+
+
+     <!-- Abschluss -->
+        <div class="mb-8 mt-8 md:flex md:justify-between items-center">
+            <div class="grid grid-cols-12 shadow md:px-6 md:py-2 w-full">
+                <div class="col-span-9 md:col-span-10 w-full">
+                    <h3 class="mb-3 font-bold text-gray-800 text-xl">Abschluss</h3>
+                    <p class="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
+
+Lasst uns also gemeinsam anstoßen auf Sebi, den Tetris-Champion, den besten Gastgeber, den Taktgeber, den Oktoberfest-Botschafter, den besten Freund und den Mann, der die Liebe gefunden hat, die er verdient! Und darauf, dass du nicht Jein gesagt hast!
+                    </p>
+                 </div>
+            </div>
+        </div>
+
+
+
+
+
+
+	</div>
+</article>
